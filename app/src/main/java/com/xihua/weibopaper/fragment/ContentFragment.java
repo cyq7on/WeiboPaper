7c2b5a320d28cadf832df5661ea4d02e2fa225dc
cyq7on <cyq7on@3f00d178-4066-410e-b2a8-a0101e431114>
2015-12-08 13:25:26 +0000

    第一次提交

---
+++ app/src/main/java/com/xihua/weibopaper/fragment/ContentFragment.java
@@ -0,0 +1,19 @@
+package com.xihua.weibopaper.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 7on on 2015/12/8.
+ */
+public class ContentFragment extends ListFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
